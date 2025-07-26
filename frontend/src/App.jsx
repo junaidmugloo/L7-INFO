0f214931bcca08e31dc579b331c5caa563179954
Junaid mugloo <junaidmugloo@gmail.com>
2025-07-26 21:49:15 +0530

    Add lazy load

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,32 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import MovieDetail from "./pages/MovieDetail";
-import ActorProfile from "./pages/ActorProfile";
-import DirectorProfile from "./pages/DirectorProfile";
+import { Suspense, lazy } from "react";
 import Navbar from "./components/Navbar";
-import AdminPanel from "./pages/AdminPanel";
-import Playground from "./pages/Playground";
-import Fav from "./pages/Fav";
+
+// Lazy-loaded pages
+const Home = lazy(() => import("./pages/Home"));
+const MovieDetail = lazy(() => import("./pages/MovieDetail"));
+const ActorProfile = lazy(() => import("./pages/ActorProfile"));
+const DirectorProfile = lazy(() => import("./pages/DirectorProfile"));
+const AdminPanel = lazy(() => import("./pages/AdminPanel"));
+const Playground = lazy(() => import("./pages/Playground"));
+const Fav = lazy(() => import("./pages/Fav"));
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/playground" element={<Playground />} />
-        <Route path="/admin" element={<AdminPanel />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/movies/:id" element={<MovieDetail />} />
-        <Route path="/actors/:id" element={<ActorProfile />} />
-        <Route path="/directors/:id" element={<DirectorProfile />} />
-        <Route path="/fav/" element={<Fav/>} />
-      </Routes>
+
+      <Suspense fallback={<div className="p-4 text-center">Loading...</div>}>
+        <Routes>
+          <Route path="/playground" element={<Playground />} />
+          <Route path="/admin" element={<AdminPanel />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/movies/:id" element={<MovieDetail />} />
+          <Route path="/actors/:id" element={<ActorProfile />} />
+          <Route path="/directors/:id" element={<DirectorProfile />} />
+          <Route path="/fav" element={<Fav />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
